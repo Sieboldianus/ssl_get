7272ef9cd05856e19558a9de453ae8f91c84ccc7
Alexander Dunkel <mail@alexanderdunkel.com>
2022-02-28 10:10:58 +0100

    fix: Script error eval ssl

--- check_cert.sh
+++ check_cert.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 # Purpose: Check expiration date of SSL in timeframe
 
-# exit on error
-set -e
-
 SCRIPT=`readlink -f "$0"`
 SCRIPTPATH=`dirname "$SCRIPT"`
 
-get_expiration_date () { 
+get_expiration_date () {
     PEM=$1
     DAYS=$2
-    openssl x509 -enddate -noout -in "$PEM"  -checkend "$DAYS" | grep -v 'Certificate will not expire'
+    /usr/bin/openssl x509 -enddate -noout -in "$PEM"  -checkend "$DAYS" | grep -v 'Certificate will not expire'
 }
 
 # Bourne shell(sh) syntax to source
 . "$SCRIPTPATH/.env"
 
-# get first entry, 
+# get first entry,
 # split by space character
 CERT_NAME=${CERT_FILES%% *}
 
 PEM="$CERT_PATH$CERT_NAME"
 
+echo "$PEM"
 # 7 days in seconds
 # DAYS="604800"
 # 14 days in seconds
@@ -33,8 +31,7 @@ echo "Checking SSL expiration date of $CERT_NAME.."
 # and if outdated
 if [ -f "$PEM" ];
 then
-    _openssl="/usr/bin/openssl"
-    $_openssl x509 -enddate -noout -in "$PEM"  -checkend "$DAYS" | grep -q 'Certificate will expire'
+    /usr/bin/openssl x509 -enddate -noout -in "$PEM"  -checkend "$DAYS" | grep -q 'Certificate will expire'
 else
     ret_val=true
 fi
@@ -43,7 +40,7 @@ fi
 if [ $? -eq 0 ]
 then
     echo "Cert not exists or will expire within $(($DAYS / 60 /60 / 24)) days. Checking for new certificate.."
-    source "$SCRIPTPATH/ftp.sh"
+    . "$SCRIPTPATH/ftp.sh"
     sleep 1
     expirationdate=$(get_expiration_date $CERT_NAME $DAYS)
     echo "Cert retrieved: $expirationdate"
